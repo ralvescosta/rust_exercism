4553da40760ea66aa6fdec5e3508f901e35cb300
Rafael Costa <rafael.rac.mg@gmail.com>
2021-06-13 09:59:36 -0300

    grains

---
+++ grains/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn square(s: u32) -> u64 {
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2u64.pow(s - 1)
+}
+
+pub fn total() -> u64 {
+    let mut total: u64 = 0;
+    for i in 1..=64 {
+        total += square(i);
+    }
+
+    total
+}
