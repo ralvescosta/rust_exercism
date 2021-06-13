da8d3c4c1403c8eff34896dc03ffe6e861ee5ea5
Rafael Costa <rafael.rac.mg@gmail.com>
2021-06-13 11:03:34 -0300

    prime factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,16 @@
+fn is_prime(n: u64) -> bool {
+    let mut count = 0;
+    for j in 1..=n {
+        if n % j == 0 {
+            count += 1;
+        }
+    }
+    if count != 2 {
+        return false;
+    }
+    return true;
+}
+
 fn recursive(n: u64, d: u64, v: &mut Vec<u64>) -> bool {
     if n == 1 {
         return true;
@@ -5,20 +18,21 @@ fn recursive(n: u64, d: u64, v: &mut Vec<u64>) -> bool {
 
     if n % d == 0 {
         v.push(d);
-        recursive(n / d, d, v);
+        return recursive(n / d, d, v);
     } else {
         return false;
     }
-
-    return true;
 }
 
 pub fn factors(n: u64) -> Vec<u64> {
     let mut facts = vec![];
 
     for i in 2.. {
-        if recursive(n, i, &mut facts) {
-            break;
+        if is_prime(i) {
+            let r = recursive(n, i, &mut facts);
+            if r {
+                break;
+            }
         }
         if i == n {
             break;
