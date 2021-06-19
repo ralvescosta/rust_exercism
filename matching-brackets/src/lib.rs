a50c303d7b6d191d6439603c98bed3ac79743537
Rafael Costa <rafael.rac.mg@gamil.com>
2021-06-19 15:52:16 -0300

    matching-brakets

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,22 +1,14 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut bracket = 0;
-    let mut braces = 0;
-    let mut parentheses = 0;
+    let mut brackets = vec![];
     for c in string.chars() {
         match c {
-            '[' => bracket += 1,
-            ']' => bracket -= 1,
-            '{' => braces += 1,
-            '}' => braces -= 1,
-            '(' => parentheses += 1,
-            ')' => parentheses -= 1,
+            '[' => brackets.push(']'),
+            '{' => brackets.push('}'),
+            '(' => brackets.push(')'),
+            ']' | '}' | ')' if brackets.pop() != Some(c) => return false,
             _ => continue,
         }
     }
 
-    if bracket == 0 && braces == 0 && parentheses == 0 {
-        return true;
-    }
-
-    false
+    brackets.is_empty()
 }
