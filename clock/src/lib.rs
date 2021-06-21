6b3a2b363410a56232004f36917b576ed074ca08
ralvescosta <rafael.rac.mg@gamil.com>
2021-06-21 07:06:46 -0300

    refactor: clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,83 +1,45 @@
-use std::fmt::*;
-#[derive(Debug, PartialEq)]
-pub struct Clock {
-    hours: i32,
-    minutes: i32,
+use std::fmt;
+
+const MINUTES_PER_HOUR: i32 = 60;
+const MINUTES_PER_DAY: i32 = 1440;
+
+fn max_minutes_per_day(time: i32) -> i32 {
+    time % MINUTES_PER_DAY
 }
 
-impl Display for Clock {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "{}:{}", self.hours, self.minutes)
-    }
-}
+#[derive(PartialEq, Debug)]
+pub struct Clock(i32);
 
-fn calc_hours(hours: i32) -> i32 {
-    match hours {
-        h if h >= 24 => hours % 24,
-        h if h < 0 && h >= -24 => h + 24,
-        h if h < 0 => (h % 24) + 24,
-        h => h,
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hours = self.0 / MINUTES_PER_HOUR;
+        let minutes = self.0 - (hours * MINUTES_PER_HOUR);
+
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut correctly_hours = calc_hours(hours);
+        let time_in_minutes = (hours * MINUTES_PER_HOUR) + minutes;
 
-        let correctly_minutes = match minutes {
-            m if m >= 60 => {
-                let ahead = m / 60;
-                if ahead >= 24 {
-                    correctly_hours += ahead % 24;
-                } else {
-                    correctly_hours += ahead;
-                }
-                m - (60 * ahead)
-            }
-            m if m < 0 => {
-                let ahead = (m * -1 / 60) + 1;
-                if ahead >= 24 {
-                    correctly_hours -= ahead % 24;
-                } else {
-                    correctly_hours -= ahead;
-                }
-                let mut min = m + (60 * ahead);
-                if min == 60 {
-                    min = 0;
-                    correctly_hours = calc_hours(correctly_hours + 1);
-                }
-                correctly_hours = calc_hours(correctly_hours);
-                min
-            }
-            m => m,
-        };
-
-        Clock {
-            hours: correctly_hours,
-            minutes: correctly_minutes,
+        if time_in_minutes < 0 {
+            return Clock(MINUTES_PER_DAY - max_minutes_per_day(time_in_minutes.abs()));
         }
+
+        Clock(max_minutes_per_day(time_in_minutes))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let hours = (self.minutes + minutes) / 60;
-        let new_minutes = (self.minutes + minutes) - (hours * 60);
-
-        Clock {
-            hours: self.hours + hours,
-            minutes: new_minutes,
+        if minutes < 0 {
+            let new_minutes = max_minutes_per_day(minutes.abs());
+            let time_in_minutes = max_minutes_per_day(self.0 - new_minutes);
+            if time_in_minutes < 0 {
+                return Clock(MINUTES_PER_DAY + time_in_minutes);
+            }
+            return Clock(time_in_minutes);
         }
-    }
 
-    pub fn to_string(&self) -> String {
-        let hours = match self.hours {
-            h if h < 10 => format!("0{}", self.hours),
-            _ => self.hours.to_string(),
-        };
-        let minutes = match self.minutes {
-            m if m < 10 => format!("0{}", self.minutes),
-            _ => self.minutes.to_string(),
-        };
-
-        String::from(format!("{}:{}", hours, minutes))
+        Clock(max_minutes_per_day(self.0 + minutes))
     }
 }
