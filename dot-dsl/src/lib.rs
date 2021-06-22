dff578a9ea6344b488760cdaf7468dfde3d94ef4
ralvescosta <rafael.rac.mg@gamil.com>
2021-06-21 21:29:06 -0300

    refactor: dot-dsl

--- dot-dsl/src/lib.rs
+++ dot-dsl/src/lib.rs
@@ -1,63 +1,65 @@
 pub mod graph {
+    use std::collections::HashMap;
+
     pub mod graph_items {
         pub mod edge {
-            #[derive(Hash, Eq, PartialEq, Debug)]
+            #[derive(Clone, Eq, PartialEq, Debug)]
             pub struct Edge;
-            impl<'a> Edge {
+            impl Edge {
                 pub fn new(s1: &str, s2: &str) -> Self {
                     Edge {}
                 }
-                pub fn with_attrs(self, attrs: &[(&'a str, &'a str)]) -> Self {
+                pub fn with_attrs(self, attrs: &[(&str, &str)]) -> Self {
                     return self;
                 }
             }
         }
 
         pub mod node {
-            #[derive(Hash, Eq, PartialEq, Debug)]
+            #[derive(Clone, Eq, PartialEq, Debug)]
             pub struct Node;
-            impl<'a> Node {
-                pub fn new(s: &'a str) -> Self {
+            impl Node {
+                pub fn new(s: &str) -> Self {
                     Node {}
                 }
-                pub fn with_attrs(self, attrs: &[(&'a str, &'a str)]) -> Self {
+                pub fn with_attrs(self, attrs: &[(&str, &str)]) -> Self {
                     return self;
                 }
             }
         }
     }
-    #[derive(Hash, Eq, PartialEq, Debug)]
-    pub struct Graph<'a> {
+    #[derive(Clone, Eq, PartialEq, Debug)]
+    pub struct Graph {
         pub nodes: Vec<graph_items::node::Node>,
         pub edges: Vec<graph_items::edge::Edge>,
-        pub attrs: Vec<(&'a str, &'a str)>,
+        pub attrs: HashMap<String, String>,
     }
 
-    impl<'a> Graph<'a> {
+    impl Graph {
         pub fn new() -> Self {
             Graph {
                 nodes: vec![graph_items::node::Node],
                 edges: vec![graph_items::edge::Edge],
-                attrs: vec![("", "")],
+                attrs: HashMap::new(),
             }
         }
 
         pub fn with_nodes(self, nodes: &Vec<graph_items::node::Node>) -> Self {
             return self;
         }
-        pub fn with_attrs(self, attrs: &[(&'a str, &'a str)]) -> Self {
+        pub fn with_attrs(self, attrs: &[(&str, &str)]) -> Self {
             return self;
         }
         pub fn with_edges(self, edges: &Vec<graph_items::edge::Edge>) -> Self {
             return self;
         }
-        pub fn get_node(self, node: &'a str) -> Self {
+        pub fn get_node(self, node: &str) -> Self {
             return self;
         }
-        pub fn expect(self, expct: &'a str) -> Self {
+        pub fn expect(self, expct: &str) -> Self {
             return self;
         }
-        pub fn get_attr(self, attr: &'a str) -> Option<&str> {
+        pub fn get_attr(self, attr: &str) -> Option<&str> {
             return Some("");
         }
     }
