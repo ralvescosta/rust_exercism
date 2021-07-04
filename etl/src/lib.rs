2b9591f05a7f52b2356a0ffa1d8ca7600eca73f8
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-04 13:51:37 -0300

    refactor: etl

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -3,14 +3,10 @@ use std::collections::BTreeMap;
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     let mut tree: BTreeMap<char, i32> = BTreeMap::new();
 
-    for n in h {
-        for letter in n.1 {
-            tree.insert(
-                *letter.to_lowercase().collect::<Vec<_>>().first().unwrap(),
-                *n.0,
-            );
+    for (&point, vec) in h {
+        for &letter in vec {
+            tree.insert(letter.to_ascii_lowercase(), point);
         }
     }
-
     return tree;
 }
