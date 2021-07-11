36f5916730a7d600c12f7da29c53b9138a9b12c5
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-11 16:54:59 -0300

    refactor: sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,24 +1,24 @@
-use std::u64;
+use std::{borrow::BorrowMut, u64};
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut primes: Vec<u64> = (2..upper_bound).map(|p| p).collect();
-    let mut index: u64 = 2;
+    let value: &mut u64 = &mut 2;
     loop {
-        if let Some(v) = mark(&mut primes, index) {
+        if let Some(v) = mark(&mut primes, value) {
             return v;
         }
-        index += 1;
     }
 }
 
-fn mark(prime: &mut Vec<u64>, index: u64) -> Option<Vec<u64>> {
+fn mark<'a>(prime: &'a mut Vec<u64>, value: &'a mut u64) -> Option<Vec<u64>> {
     let mut amount = 0;
-    for p in prime.into_iter() {
-        if *p == index {
+
+    for (i, p) in prime.into_iter().enumerate() {
+        if *p == *value {
             continue;
         }
 
-        if *p % index == 0 {
+        if *p % *value == 0 {
             amount += *p;
             *p = 0;
         }
