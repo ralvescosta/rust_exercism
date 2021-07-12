a326e2a840e34fc73d83ae1b1b271d6de80661a2
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-12 06:13:59 -0300

    refactor: sieve

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,35 +1,37 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut primes: Vec<u64> = (2..upper_bound).map(|p| p).collect();
-    let value: &mut u64 = &mut 2;
-    loop {
-        if let Some(v) = mark(&mut primes, value) {
-            return v;
-        }
-    }
+    let mut primes: Vec<u64> = (2..=upper_bound).map(|p| p).collect();
+    return mark(&mut primes, 2);
 }
 
-fn mark<'a>(prime: &'a mut Vec<u64>, value: &'a mut u64) -> Option<Vec<u64>> {
+fn mark<'a>(prime: &'a mut Vec<u64>, value: u64) -> Vec<u64> {
     let mut amount = 0;
+    let mut get_next_value = false;
+    let mut next_value = 0;
 
-    for (i, p) in prime.into_iter().enumerate() {
-        if *p == *value {
+    for p in prime.into_iter() {
+        if *p == value {
+            get_next_value = true;
             continue;
         }
 
-        if *p % *value == 0 {
+        if get_next_value && *p > value {
+            next_value = *p;
+            get_next_value = false;
+        }
+
+        if *p % value == 0 {
             amount += *p;
             *p = 0;
         }
     }
 
-    if amount == 0 {
-        return Some(
-            prime
-                .iter()
-                .filter(|&&p| p != 0)
-                .map(|&p| p)
-                .collect::<Vec<u64>>(),
-        );
+    if amount > 0 {
+        return mark(prime, next_value);
     }
-    return None;
+
+    return prime
+        .iter()
+        .filter(|&&p| p != 0)
+        .map(|&p| p)
+        .collect::<Vec<u64>>();
 }
