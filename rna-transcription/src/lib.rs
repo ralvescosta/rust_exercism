a0e76427c50b9fd6caca26ae5cb4db27e073a9b2
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-12 07:20:19 -0300

    refactor: rna-transcription

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,45 @@
-#[derive(Debug, PartialEq)]
-pub struct Dna;
+use std::usize;
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Dna(String);
+
+#[derive(Debug, PartialEq)]
+pub struct Rna(String);
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        for (i, n) in dna.chars().into_iter().enumerate() {
+            if n == 'A' || n == 'C' || n == 'G' || n == 'T' {
+                continue;
+            }
+            return Err(i);
+        }
+        return Ok(Dna(String::from(dna)));
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        Rna(self
+            .0
+            .chars()
+            .map(|n| match n {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => panic!(""),
+            })
+            .collect())
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        for (i, n) in rna.chars().into_iter().enumerate() {
+            if n == 'A' || n == 'C' || n == 'G' || n == 'U' {
+                continue;
+            }
+            return Err(i);
+        }
+        return Ok(Rna(String::from(rna)));
     }
 }
