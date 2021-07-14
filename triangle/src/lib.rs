2fe6ed19c9638ffce4cf7c26d12511de131e6e23
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-14 06:15:45 -0300

    refactor: triangle

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,22 +1,27 @@
+use std::ops::Add;
 pub struct Triangle<T>(T, T, T);
 
-impl<T> Triangle<T> {
+impl<T: Copy + PartialOrd + Add<Output = T>> Triangle<T> {
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
-        if let Some(i) = sides.iter().find(|&&s| s < 0) {
-            None
+        let a = sides[0];
+        let b = sides[1];
+        let c = sides[2];
+
+        if a + b > c && b + c > a && a + c > b {
+            return Some(Triangle(a, b, c));
         }
         return None;
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        self.0 == self.1 && self.1 == self.2
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        self.0 != self.1 && self.0 != self.2 && self.1 != self.2
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+        self.0 == self.1 || self.0 == self.2 || self.1 == self.2
     }
 }
