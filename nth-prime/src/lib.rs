b6bbf503d1916081470817cf346f153e53333552
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-14 07:14:26 -0300

    refactor: nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,7 +1,25 @@
 pub fn nth(n: u32) -> u32 {
-    prime_numer(n)
-}
+    let n = n + 1;
+    let limit = 1_000_000;
+    let mut sieve = vec![true; limit];
+    let mut count = 0;
 
-fn prime_numer(n: u32) -> u32 {
-    nth(n)
+    sieve[0] = false;
+    sieve[1] = false;
+
+    for prime in 2..limit {
+        if !sieve[prime] {
+            continue;
+        }
+        count += 1;
+        if count == n {
+            return prime as u32;
+        }
+
+        for multiple in ((prime * prime)..limit).step_by(prime) {
+            sieve[multiple] = false;
+        }
+    }
+
+    panic!("impossible prime number for this algorithm");
 }
--- nth-prime/src/main.rs
+++
@@ -1,9 +0,0 @@
-use nth_prime::nth;
-
-
-fn main() {
-  for i in 2..{
-    println!("{}", i)
-
-  }
-}
