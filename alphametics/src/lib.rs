b9354565d8d83b6b6018429a6cce6d691fcd2af4
ralvescosta <rafael.rac.mg@gmail.com>
2021-07-23 20:05:11 -0300

    feat: alphametrics

---
+++ alphametics/src/lib.rs
@@ -0,0 +1,7 @@
+use std::collections::HashMap;
+
+pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
+    //O = 0, M = 1, Y = 2, E = 5, N = 6, D = 7, R = 8, and S = 9.
+    
+    Some(HashMap::new())
+}
